60cce90ec2d201b6cc8cf65cc0fb0e2f46e860c2
HeitorFernandes <heitorsoaresfer@gmail.com>
2022-08-08 17:32:37 -0300

    bugs node_module

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -9,21 +9,25 @@ class Feedback extends React.Component {
 
 
   render() {
-   const { score } = this.props;
+   const { assertions, score } = this.props;
    return(
-    <div data-testid='feedback-text'>
-    {score < 3 ? (<span>Could be better...</span>) 
-    : (<span>Well Done!</span>) }
+    <div>
+    {score < 3 ? (<span data-testid='feedback-text'>Could be better...</span>) 
+    : (<span data-testid='feedback-text'>Well Done!</span>) }
+    <span data-testid>{ assertions }</span>
+    <span data-testid>{ score }</span>
     </div>
    )
   }
 }
 
 Feedback.propTypes = {
+  assertions: PropTypes.number.isRequired,
   score: PropTypes.number.isRequired,
 };
 
 const mapStateToProps = (state) => ({
+    assertions: state.player.score,
     score: state.player.score,
   });
 
