061e6cd57c3e11087734f54948530f45fa945d10
Henos <henosvinicius13@gmail.com>
2022-08-10 15:22:28 -0300

    mocks para os testes

---
+++ src/tests/helpers/mockedFunctions.js
@@ -0,0 +1,21 @@
+import requestTokenObj from '../../services/requestToken';
+import requestQuestionsObj from '../../services/requestQuestions';
+import { mockedToken, mockedQuestions } from './mockedData';
+
+export const mockedReqToken = jest.spyOn(requestTokenObj, 'requestToken').mockImplementation(async () => {
+  const data = await Promise.resolve({
+    ...mockedToken,
+  });
+  localStorage.setItem('token', data.token);
+});
+
+export const mockedReqQuestions = jest.spyOn(requestQuestionsObj, 'requestQuestions').mockImplementation(async (token) => {
+  const url = `https://opentdb.com/api.php?amount=5&token=${token}`
+  const falseFetch = async (url) => {
+   return await Promise.resolve({
+    ...mockedQuestions,
+   });
+  };
+  const data = falseFetch(url);
+  return data;
+});
