8c33f6ee42f687c8e021744cefb1c6c610608c72
Victor-Murilo <victor2706.vm@gmail.com>
2022-08-10 18:20:24 -0300

    Resolvendo os problemas dos testes

--- src/services/requestQuestions.js
+++ src/services/requestQuestions.js
@@ -1,8 +1,14 @@
 const defaultValue = 5;
 
 const requestQuestionsObj = {
-  requestQuestions: async (token, { category = '', difficulty = '', type = '' }) => {
-    const url = `https://opentdb.com/api.php?amount=${defaultValue}&category=${category}&difficulty=${difficulty}&type=${type}&token=${token}`;
+  requestQuestions: async (token, { category, difficulty, type }) => {
+    console.log(category, ' ', difficulty, ' ', type);
+    let url = '';
+    if (category === '' && difficulty === '' && type === '') {
+      url = `https://opentdb.com/api.php?amount=${defaultValue}&token=${token}`;
+    } else {
+      url = `https://opentdb.com/api.php?amount=${defaultValue}&category=${category}&difficulty=${difficulty}&type=${type}&token=${token}`;
+    }
     const response = await fetch(url);
     const data = await response.json();
     return data;
