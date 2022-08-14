b18b48c89c7336ec5912bad23b4393da78516d52
Henos <henosvinicius13@gmail.com>
2022-08-14 15:40:50 -0300

    Estilização da página Ranking

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import md5 from 'crypto-js/md5';
 import PlayAgainBtn from '../components/PlayAgainBtn';
 import HeaderAipim from '../components/HeaderAipim';
+import '../styles/Ranking.css';
 
 class Ranking extends React.Component {
   constructor() {
@@ -27,29 +28,42 @@ class Ranking extends React.Component {
     return (
       <>
         <HeaderAipim />
-        <div>
-          <h1 data-testid="ranking-title">Ranking</h1>
-          {
-            playersInfo.sort((a, b) => Number(b.score) - Number(a.score))
-              .map((player, index) => (
-                <div key={ `${player}${index}` }>
-                  <img src={ `https://www.gravatar.com/avatar/${md5(player.gravatarEmail).toString()}` } alt="Imagem Gravatar" />
-                  <p>
-                    Player:
-                    {' '}
-                    <span data-testid={ `player-name-${index}` }>{player.name}</span>
-                  </p>
-                  <p>
-                    Score:
-                    {' '}
-                    <span data-testid={ `player-score-${index}` }>{player.score}</span>
-                  </p>
-                </div>
-
-              ))
-          }
-          <PlayAgainBtn history={ history } testid="btn-go-home" />
-        </div>
+        <main className="ranking-body">
+          <div className="ranking-container">
+            <h1 data-testid="ranking-title">Ranking</h1>
+            <div className="ranking-people">
+              {
+                playersInfo.sort((a, b) => Number(b.score) - Number(a.score))
+                  .map((player, index) => (
+                    <div className="people-info" key={ `${player}${index}` }>
+                      <img src={ `https://www.gravatar.com/avatar/${md5(player.gravatarEmail).toString()}` } alt="Imagem Gravatar" />
+                      <div>
+                        <p>
+                          Player:
+                          {' '}
+                          <span
+                            data-testid={ `player-name-${index}` }
+                          >
+                            {player.name}
+                          </span>
+                        </p>
+                        <p>
+                          Score:
+                          {' '}
+                          <span
+                            data-testid={ `player-score-${index}` }
+                          >
+                            {player.score}
+                          </span>
+                        </p>
+                      </div>
+                    </div>
+                  ))
+              }
+            </div>
+            <PlayAgainBtn history={ history } testid="btn-go-home" />
+          </div>
+        </main>
       </>
     );
   }
